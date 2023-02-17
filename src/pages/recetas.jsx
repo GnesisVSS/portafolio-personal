ce0e43e23d12fffce58344988d52064e9e04c289
Genesis Saez <gnesis.vss@gmail.com>
2023-02-17 19:12:02 -0300

    Correccion tamaño de elementos

--- src/pages/recetas.jsx
+++ src/pages/recetas.jsx
@@ -28,27 +28,25 @@ const Recetas = () => {
                 <div className="container col-sm-12 py-2 mx-auto">
                     <div className="row justify-content-center align-items-center py-5">
 
-                        <div className="col-sm-8 col-md-5 px-5">
-                            <div className="col text-black">
-                                <h1 className="text-focus-in titulo-inicio" style={estilo1}>Bienvenido(a)!</h1>
-                                <p style={estilo2}>
-                                    Recetas dulces, saladas, saludables, para todos los gustos! Accede a estas y más recetas
-                                    iniciando sesión, si no tienes una cuenta puedes registrarte gratis, para acceder al listado
-                                    completo de
-                                    resetas que tenemos para ti. Además tambien puedes crear tus propias recetas y guardarlas
-                                    junto a las que te entregamos
-                                    para tener una mayor variedad.
-                                </p>
-                                <div style={estilo3}>
-                                    <a href="/login" type="button" className="button-inicio">Inicia sesión</a>
-                                </div>
+                        <div className="col-sm-8 col-md-5">
+                            <h1 className="text-focus-in titulo-inicio" style={estilo1}>Bienvenido(a)!</h1>
+                            <p style={estilo2}>
+                                Recetas dulces, saladas, saludables, para todos los gustos! Accede a estas y más recetas
+                                iniciando sesión, si no tienes una cuenta puedes registrarte gratis, para acceder al listado
+                                completo de
+                                resetas que tenemos para ti. Además tambien puedes crear tus propias recetas y guardarlas
+                                junto a las que te entregamos
+                                para tener una mayor variedad.
+                            </p>
+                            <div style={estilo3}>
+                                <a href="/login" type="button" className="button-inicio">Inicia sesión</a>
                             </div>
                         </div>
 
 
                         <div className="col-sm-8 col-md-6 px-5 py-4">
                             <div className="col text-black">
-                                <img src="../img/dibujo-cocina.svg" alt="" className='img-fluid'/>
+                                <img src="../img/dibujo-cocina.svg" alt="" className='img-fluid' />
                             </div>
                         </div>
 
