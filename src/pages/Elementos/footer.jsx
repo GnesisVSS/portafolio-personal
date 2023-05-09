783ae36a2fbc5e8137df32c59f393b57f97b65a7
Genesis Saez <gnesis.vss@gmail.com>
2023-05-09 15:28:58 -0400

    correccion de visualizacion de elementos en Footer

--- src/pages/Elementos/footer.jsx
+++ src/pages/Elementos/footer.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="container text-center">
+        <div className="container py-5">
             <div className="row align-items-start">
-                <div className="col text-light">
+                <div className="col-12 text-light py-2">
                     gnesis.vss@gmail.com
                 </div>
-                <div className="col text-light">
+                <div className="col-12 text-light py-2">
                     <a>
                         mi Portafolio
                     </a>
                 </div>
-                <div className="col text-light">
+                <div className="col-12 text-light py-2">
                     © Genesis Sáez
                 </div>
             </div>
