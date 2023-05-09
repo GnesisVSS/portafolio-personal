afdb3ced74f8d192fe62e8c533f61da6cf83cdba
Genesis Saez <gnesis.vss@gmail.com>
2023-05-09 15:47:39 -0400

    correccion visualizacion de formulario

--- src/pages/Home/ContactForm.jsx
+++ src/pages/Home/ContactForm.jsx
@@ -27,81 +27,73 @@ const ContactForm = () => {
     };
 
     return (
-        <section style={{ backgroundColor: '#EDE6F1', color: '#162E36' }} className='py-5'>
-            <form onSubmit={handleSubmit}>
+        <section  style={{ backgroundColor: '#EDE6F1', color: '#162E36' }} className='align-items-center'>
 
-                <div className="container">
-                    <div className="row justify-content-start">
-                        <strong><h1 style={{ textAlign: 'left', color: '#162E36' }} className='h1Proyectos'>Contactame</h1></strong>
+            <div>
+                <div className="container col-sm-8 mx-auto">
+                    <div className="row justify-content-center">
+                        <div className="col-sm-8 col-md-8 text-white align-self-center" >
+                            <form onSubmit={handleSubmit} >
 
-                        <div className="col-lg-12 py-4">
-                            <TextField
-                                type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                label="Nombre"
-                                variant="standard"
-                                color="secondary"
-                                focused
-                                style={{ width: '100%' }}
-                            />
+                                <strong><h1 style={{ textAlign: 'left', color: '#162E36' }} className='h1Proyectos'>Contactame</h1></strong>
 
+                                <div className="col-sm-12 col-md-12 py-4">
+                                    <TextField
+                                        type="text"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        label="Nombre"
+                                        variant="standard"
+                                        color="secondary"
+                                        focused
+                                        style={{ width: '100%' }}
+                                    />
+
+                                </div>
+                                <div className="col-sm-12 col-md-12 py-4">
+                                    <TextField
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        label="Email"
+                                        variant="standard"
+                                        color="secondary"
+                                        focused
+                                        style={{ width: '100%' }}
+                                    />
+                                </div>
+                                <div className="col-sm-12 col-md-12 py-4">
+                                    <TextField
+                                        value={message}
+                                        onChange={(e) => setMessage(e.target.value)}
+                                        color="secondary"
+                                        focused
+                                        label="Mensaje"
+                                        multiline
+                                        rows={6}
+                                        style={{ backgroundColor: 'white', width: '100%' }}
+                                    />
+
+                                </div>
+
+                                <div style={{ textAlign: 'center' }} className='py-4'>
+                                    <button className="noselect butcorreo" type='submit'>
+                                        <span className="textCorreo spanCorreo">Enviar</span>
+                                        <span className="iconCorreo spanCorreo">
+                                            <MailOutlineIcon className='svgCorreo' width="48" height="48" />
+                                        </span>
+                                    </button>
+                                </div>
+
+                            </form>
                         </div>
-                        <div className="col-lg-12 py-4">
-                            <TextField
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                label="Email"
-                                variant="standard"
-                                color="secondary"
-                                focused
-                                style={{ width: '100%' }}
-                            />
-                        </div>
-                        <div className="col-lg-12 py-4">
 
-                            {/* <TextField
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                id="filled-multiline-static"
-                                label="Mensaje"
-                                multiline
-                                rows={4}
-                                variant="filled"
-                                color="secondary"
-                                style={{ backgroundColor: 'white', border: '1px solid #9c27b0', width: '100%' }}
-                                focused
-                            /> */}
-                            <TextField
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                color="secondary"
-                                focused
-                                label="Mensaje"
-                                multiline
-                                rows={6}
-                                style={{ backgroundColor: 'white', width: '100%' }}
-                            />
-
-                        </div>
-
-
-                    </div>
-                    <div style={{ textAlign: 'center' }} className='py-4'>
-                        <button className="noselect butcorreo" type='submit'>
-                            <span className="textCorreo spanCorreo">Enviar</span>
-                            <span className="iconCorreo spanCorreo">
-                                <MailOutlineIcon className='svgCorreo' width="48" height="48" />
-                            </span>
-                        </button>
                     </div>
                 </div>
+            </div >
 
-            </form>
 
-        </section>
-
+        </section >
     );
 }
 
