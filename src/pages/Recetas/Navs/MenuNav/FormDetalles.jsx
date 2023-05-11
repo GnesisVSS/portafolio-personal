3845108490cc019b7a6f909549935e123b147edf
Genesis Saez <gnesis.vss@gmail.com>
2023-05-11 16:50:11 -0400

    se agregan las validaciones correspondientes con Formik a la vista de Detalles

--- src/pages/Recetas/Navs/MenuNav/FormDetalles.jsx
+++ src/pages/Recetas/Navs/MenuNav/FormDetalles.jsx
@@ -2,19 +2,20 @@
 /* eslint-disable react/prop-types */
 import { Box, Fade, Modal, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { Field, useFormik } from 'formik';
+import { ErrorMessage, Field, useFormik } from 'formik';
 import FormIngredientes from './FormIngredientes';
 import Photo from '../../Photo';
 import { registroDetalleRecetaUsuario, registroIngredientes, registroRecetasUsuario } from '../../../../api/receta.api';
 import { Navigate, useNavigate } from 'react-router-dom';
 import Backdrop from '@mui/material/Backdrop';
+import * as Yup from 'yup';
+import Cleave from 'cleave.js/react';
 
 const FormDetalles = (datosIngredientes, props) => {
     const [valorInput, setValorInput] = useState('');
     const correousuario = localStorage.getItem("correoUsuario");
 
     const valuesDetalle = datosIngredientes.datosIngredientes[0]
-    const [success, setSuccess] = useState(false);
     useEffect(() => {
         setValorInput(props.valorInicial);
         console.log(valuesDetalle.length)
@@ -26,25 +27,39 @@ const FormDetalles = (datosIngredientes, props) => {
         setInfo(newInfo);
     };
 
-    const [open, setOpen] = React.useState(false);
-
-
-    const handleClose = () => {
-        setOpen(false);
+    const handleKeyPress = (event) => {
+        // Obtiene el código de tecla ingresado
+        const keyCode = event.keyCode || event.which;
+        const character = String.fromCharCode(keyCode);
+        // Expresión regular para letras y números
+        const pattern = /^[a-zA-Z0-9]*$/;
+        // Verifica si el código de tecla corresponde a una letra
+        if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) || (!pattern.test(character))) {
+            // Si es una letra, cancela el evento de teclado
+            event.preventDefault();
+        }
     };
 
+    const valSchema = Yup.object().shape({
+        nombre: Yup.string()
+            .required('El nombre es obligatorio')
+            .min(5, 'El nombre es muy corto')
+            .max(120, 'El nombre excede el máximo de caracteres'),
+        descripcion: Yup.string()
+            .required('La descripcion es obligatoria')
+            .min(5, 'La descripcion es muy corta'),
+        preparacion: Yup.string()
+            .required('La preparacion es obligatoria')
+            .min(10, 'La preparacion es muy corta'),
+        tiempoPreparacion: Yup.number()
+            .required('El tiempo de preparacion es obligatorio')
+            .min(1, 'El tiempo minimo de preparación es de 1 min'),
+        porciones: Yup.number()
+            .required('Las porciones son obligatorias')
+            .min(1, 'El minimo de porciones es de 1')
+            .max(100, 'El maximo de porciones es de 100'),
+    })
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        bgcolor: 'background.paper',
-        boxShadow: 24,
-        p: 4,
-    };
-
-    const navigate = useNavigate();
     const formik = useFormik({
         initialValues: {
             nombre: "",
@@ -56,20 +71,20 @@ const FormDetalles = (datosIngredientes, props) => {
             dificultad: "",
             imgUrl: ""
         },
+        validationSchema: valSchema,
         onSubmit: async (detallesIngredientes) => {
             detallesIngredientes.imgUrl = info;
             try {
-                const response = await registroIngredientes(valuesDetalle)
-                const responseDetalle = await registroDetalleRecetaUsuario(detallesIngredientes)
-                console.log(response)
-                console.log(responseDetalle)
+                await registroIngredientes(valuesDetalle)
+                await registroDetalleRecetaUsuario(detallesIngredientes)
+                localStorage.removeItem("formValues");
                 window.location.href = '/misRecetas';
 
             } catch (error) {
                 console.log(error)
 
             }
-        },
+        }
     });
 
 
@@ -77,7 +92,7 @@ const FormDetalles = (datosIngredientes, props) => {
     const handleClick = (event, index) => {
         setPage(!page);
     }
-    // console.log(datosIngredientes)
+
 
 
     return (
@@ -87,10 +102,12 @@ const FormDetalles = (datosIngredientes, props) => {
                     <h1 className='text-focus-in titulo-inicio'>Detalles</h1>
                     <div className='py-4'>
                         <div className="input-group mb-3">
-                            <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='nombre' onChange={formik.handleChange} id="outlined-basic" label="Nombre" variant="outlined" />
+                            <TextField error={formik.touched.nombre && Boolean(formik.errors.nombre)} onBlur={formik.handleBlur}
+                                helperText={formik.touched.nombre && formik.errors.nombre} fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='nombre' onChange={formik.handleChange} id="nombre" label="Nombre" variant="outlined" />
                         </div>
                         <div className="input-group mb-3">
-                            <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='descripcion' onChange={formik.handleChange} id="outlined-basic" label="Descripcion" variant="outlined" />
+                            <TextField error={formik.touched.descripcion && Boolean(formik.errors.descripcion)} onBlur={formik.handleBlur}
+                                helperText={formik.touched.descripcion && formik.errors.descripcion} fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='descripcion' onChange={formik.handleChange} id="outlined-basic" label="Descripcion" variant="outlined" />
                         </div>
                         <div className="input-group mb-3">
                             {/* <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='preparacion' onChange={formik.handleChange} id="outlined-basic" label="Preparacion" variant="outlined" /> */}
@@ -101,16 +118,21 @@ const FormDetalles = (datosIngredientes, props) => {
                                 label="Preparacion"
                                 multiline
                                 rows={6}
+                                error={formik.touched.preparacion && Boolean(formik.errors.preparacion)} 
+                                onBlur={formik.handleBlur}
+                                helperText={formik.touched.preparacion && formik.errors.preparacion}
                             />
                         </div>
                         <div className="input-group mb-3">
                             <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='categoria' onChange={formik.handleChange} id="outlined-basic" label="Categoria" variant="outlined" />
                         </div>
                         <div className="input-group mb-3">
-                            <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='tiempoPreparacion' onChange={formik.handleChange} id="outlined-basic" label="Tiempo Preparacion" variant="outlined" />
+                            <TextField error={formik.touched.tiempoPreparacion && Boolean(formik.errors.tiempoPreparacion)} onBlur={formik.handleBlur}
+                                helperText={formik.touched.tiempoPreparacion && formik.errors.tiempoPreparacion} onKeyPress={handleKeyPress} fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='tiempoPreparacion' onChange={formik.handleChange} id="outlined-basic" label="Tiempo Preparacion" variant="outlined" />
                         </div>
                         <div className="input-group mb-3">
-                            <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='porciones' onChange={formik.handleChange} id="outlined-basic" label="Porciones" variant="outlined" />
+                            <TextField error={formik.touched.porciones && Boolean(formik.errors.porciones)} onBlur={formik.handleBlur}
+                                helperText={formik.touched.porciones && formik.errors.porciones} fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='porciones' onChange={formik.handleChange} id="outlined-basic" label="Porciones" variant="outlined" />
                         </div>
                         <div className="input-group mb-3">
                             <TextField fullWidth sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }} name='dificultad' onChange={formik.handleChange} id="outlined-basic" label="Dificultad" variant="outlined" />
@@ -133,6 +155,8 @@ const FormDetalles = (datosIngredientes, props) => {
 
 
                 </form>
+                {/* <Field name="creditCard" component={renderCleaveField} /> */}
+
                 <div style={{ textAlign: "right" }}>
                     <button type="submit" className="button-inicio" name="formSub" id="formSub" value="form" onClick={handleClick}>
                         {/* <span >Loading...</span> */}
