687c96c4515fc5500d722a4bc5641aa73655a5c9
Genesis Saez <gnesis.vss@gmail.com>
2023-08-18 17:34:09 -0400

    correccion link y cursor

--- src/pages/Home/proyectos.jsx
+++ src/pages/Home/proyectos.jsx
@@ -9,28 +9,9 @@ import { Box } from '@mui/material';
 import { IconChefHat } from '@tabler/icons-react';
 import { useState } from "react";
 
-const menu = ["Página web de recetas", "prueba"]
+const menu = ["Página web de recetas", "Rick y Morty"]
 const links = ["/RecetasHome", "#"]
-console.log(menu.length)
-
-
-function mostrarImagen() {
-    var imagen = document.getElementById('imagen');
-    var imagenD = document.getElementById('imagenDefecto');
-    // console.log(imagen.)
-    imagen.style.display = 'block';
-    imagenD.style.display = 'none';
-}
-
-function ocultarImagen() {
-    var imagen = document.getElementById('imagen');
-    var imagenD = document.getElementById('imagenDefecto');
-    // console.log(imagen.)
-    imagen.style.display = 'none';
-    imagenD.style.display = 'block';
-}
-
-
+const imagen = ["/img/Shawarma.gif", "/img/rickymorty.jpg"]
 
 const show = {
     opacity: 1,
@@ -46,52 +27,69 @@ const hide = {
 
 
 const Proyectos = () => {
+    const [indexImagen, setIndexImagen] = useState([]);
 
+    function mostrarImagen() {
+        var imagen = document.getElementById('imagen');
+        var imagenD = document.getElementById('imagenDefecto');
+        imagen.style.display = 'block';
+        imagenD.style.display = 'none';
+
+    }
+
+    function ocultarImagen() {
+        var imagen = document.getElementById('imagen');
+        var imagenD = document.getElementById('imagenDefecto');
+        imagen.style.display = 'none';
+        imagenD.style.display = 'block';
+
+    }
+    const handleListItemClick = (index,e) => {
+        e.preventDefault();
+        window.location.href = links[index];
+    };
     function renderRow(props) {
         // eslint-disable-next-line react/prop-types
         const { index, style } = props;
 
         return (
-            <List style={style} key={index} component="div" disablePadding>
-                <ListItem alignItems="flex-start" >
-
-                    {/* <ListItemText
-                            primary={menu[index]}
-                        /> */}
-
-                    {/* <h2 className='titulo-demo'>Web de recetas</h2> */}
-                    <h1 style={{ width: '500px' }} >
-
+            <motion.div
+                whileTap={{ scale: 0.95 }}
+                onMouseEnter={() => {
+                    setIndexImagen(prevIndex => {
+                        mostrarImagen(index);
+                        return [index];
+                    });
+                    setIsVisible(false);
+                }}
+                onMouseLeave={() => {
+                    setIsVisible(true);
+                    ocultarImagen();
+                }}
+            >
+                <List style={style} key={index} component="div" disablePadding>
+                    <ListItem alignItems="flex-start" style={{ textDecoration: 'none',color:'#162E36',cursor:'pointer' }}  onClick={(e) => handleListItemClick(index, e)}> 
                         <div className="controls">
-                            <IconChefHat width='40px' height='40px' style={{ paddingRight: '10px' }} />
-                            <motion.a
-                                href={links[index]}
-                                whileTap={{ scale: 0.95 }}
-                                style={{ textDecoration: 'none' }} className='menuProyectos'
-                                onMouseEnter={() => {
-                                    setIsVisible(false);
-                                    mostrarImagen();
-                                }}
-                                onMouseLeave={() => {
-                                    setIsVisible(true);
-                                    ocultarImagen();
-                                }}
-                            >
-                                {menu[index]}
-                            </motion.a>
+                        
+                            <a >
+                                <h2
+                                    className='titulo-demo'
+                                >
+                                    <IconChefHat width='38px' height='38px' style={{ paddingRight: '10px' }} />
+                                    {menu[index]}
+                                </h2>
+                            </a>
+
                         </div>
-                        {/* <a href={links[index]} style={{ textDecoration: 'none' }} className='menuProyectos' onMouseOver={mostrarImagen} onMouseLeave={ocultarImagen}>
-                            {menu[index]}
-                        </a> */}
-                    </h1>
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                </List>
+            </motion.div>
 
-
-                </ListItem>
-                <Divider variant="inset" component="li" />
-            </List>
 
         );
     }
+
     const estilo2 = {
         fontSize: '18px'
     }
@@ -182,20 +180,10 @@ const Proyectos = () => {
 
                                 </div>
                             </div>
-                            {/* 
-                            <div className="controls">
-                                <motion.a
-                                    whileTap={{ scale: 0.95 }}
-                                    onMouseEnter={() => setIsVisible(!isVisible)}
-                                    onMouseLeave={() => setIsVisible(!isVisible)}
-                                >
-                                    {isVisible ? "Hide" : "Show"}
-                                </motion.a>
-                            </div> */}
                             <div className='col-sm-6 col-md-6  align-self-center' >
                                 <motion.div animate={isVisible ? hide : show} >
-                                    <div className="col">
-                                        <img src='/img/Shawarma.gif' style={{ borderRadius: '40px', height: '600px', width: '600px' }} id='imagen' />
+                                    <div className="col" >
+                                        <img src={imagen[indexImagen[0]]} style={{ borderRadius: '40px', height: '600px', width: '600px' }} id='imagen' />
                                     </div>
                                 </motion.div>
                                 <motion.div animate={isVisible ? show : hide} >
