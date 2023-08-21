9aefdb19d0a7e027f0d67e5acc3d53cf2eb945b2
Genesis Saez <gnesis.vss@gmail.com>
2023-08-21 14:47:11 -0400

    finalizacion de correcciones en vista pre demo

--- src/pages/Recetas/VistaPreDemo.jsx
+++ src/pages/Recetas/VistaPreDemo.jsx
@@ -10,6 +10,8 @@ import { IconBrandJavascript } from '@tabler/icons-react';
 import { IconBrandGit } from '@tabler/icons-react';
 import { IconBrandCss3 } from '@tabler/icons-react';
 import { IconBrandHtml5 } from '@tabler/icons-react';
+import Navbar from '../Elementos/navbar';
+import Footer from '../Elementos/footer';
 
 const VistaPreDemo = () => {
 
@@ -33,17 +35,21 @@ const VistaPreDemo = () => {
             control.start("hidden");
         }
     }, [control, inView]);
-
+    let ubicacionPrincipal = window.pageYOffset;
+    window.onscroll = function () {
+        let Desplazamiento_actual = window.pageYOffset;
+        if (ubicacionPrincipal >= Desplazamiento_actual) {
+            document.getElementById('navbar').style.top = '0px';
+        } else {
+            document.getElementById('navbar').style.top = '-100px';
+        }
+        ubicacionPrincipal = Desplazamiento_actual;
+    }
     return (
-        <section id='recetas' style={{ backgroundColor: '#EDE6F1', color: '#162E36', minHeight: '100vh' }}>
-            <motion.div
-                ref={ref}
-                variants={boxVariant}
-                initial="hidden"
-                animate={control}
-
-            >
-                <div className='py-5'>
+        <section id='recetas' style={{ minHeight: '100vh' }}>
+            <div style={{ backgroundColor: '#EDE6F1', color: '#162E36' }}>
+                <Navbar />
+                <div className='py-5' >
                     <div className="container col-sm-12 py-2 mx-auto">
 
                         <div className="row justify-content-center">
@@ -51,7 +57,7 @@ const VistaPreDemo = () => {
                             <div className="col-sm-8 col-md-6 p-5 align-self-center">
 
                                 <div className="col" style={{ lineHeight: '27px' }}>
-                                    <strong><h1 style={{ textAlign: 'left', color: '#162E36' }} className='h1Proyectos'>Pagina web de Recetas</h1></strong>
+                                    <strong><h1 style={{ textAlign: 'left', color: '#162E36', marginTop: '40px' }} className='h1Proyectos'>Pagina web de Recetas</h1></strong>
                                     <div className='py-4'>
                                         <p style={estilo2}>
                                             Pagina web ficticia en donde se puede leer información de diferentes recetas con su respectiva información, tales como: porciones, tiempo de preparación,categoría,Ingredientes y detalles de la preparación.
@@ -120,48 +126,58 @@ const VistaPreDemo = () => {
 
                             </div>
                         </div>
-                        <Table aria-label="basic table"  sx={{ '& thead th:nth-child(1)': { width: '40%' } }}>
-                            <tbody>
-                                <tr>
-                                    <td> <strong>Lenguaje de Programación</strong></td>
-                                    <td><IconBrandJavascript width='32px' height='32px' style={{ color: '#ffd630', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} /></td>
-                                    <td>JavaScript</td>
-                                </tr>
-                                <tr>
-                                    <td> <strong>Framework</strong> </td>
-                                    <td><IconBrandReact width='32px' height='32px' style={{ color: '#61DBFB', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} /></td>
-                                    <td>ReactJS</td>
-                                </tr>
-                                <tr>
-                                    <td> <strong>Base de Datos</strong> </td>
-                                    <td><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-planetscale" width='32px' height='32px' viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" style={{ color: 'black', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }}>
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M20.993 11.63a9 9 0 0 1 -9.362 9.362l9.362 -9.362z"></path>
-                                        <path d="M12 3a9.001 9.001 0 0 1 8.166 5.211l-11.955 11.955a9 9 0 0 1 3.789 -17.166z"></path>
-                                        <path d="M12 12l-6 6"></path>
-                                    </svg></td>
-                                    <td>MySql con PlanetScale</td>
-                                </tr>
-                                <tr>
-                                    <td> <strong>Complementos</strong> </td>
-                                    <td><IconBrandGit width='32px' height='32px' style={{ color: '#f96926', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} /></td>
-                                    <td>Git</td>
-                                </tr>
-                                <tr>
-                                    <td> <strong></strong> </td>
-                                    <td><IconBrandHtml5 width='32px' height='32px' style={{ color: '#f96926', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} /></td>
-                                    <td>HTML</td>
-                                </tr>
-                                <tr>
-                                    <td> <strong></strong> </td>
-                                    <td><IconBrandCss3 width='32px' height='32px' style={{ color: '#61DBFB', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} /></td>
-                                    <td>CSS</td>
-                                </tr>
-                            </tbody>
-                        </Table>
+
+
                     </div>
                 </div>
-            </motion.div>
+                <div className="container text-center" style={{ paddingBottom: '32px' }}>
+                    <div className="row" style={{ marginBottom: '32px' }}>
+                        <div className="col-md-10 offset-md-1 text-start">
+                            <h1 style={{ marginBottom: '32px' }}>Especificaciones</h1>
+                            <Table aria-label="basic table">
+                                <tbody style={{ fontSize: '18px' }}>
+                                    <tr>
+                                        <td> <strong>Lenguaje de Programación</strong></td>
+                                        <td></td>
+                                        <td><IconBrandJavascript width='32px' height='32px' style={{ color: '#ffd630', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} />JavaScript</td>
+                                    </tr>
+                                    <tr>
+                                        <td> <strong>Framework</strong> </td>
+                                        <td></td>
+                                        <td><IconBrandReact width='32px' height='32px' style={{ color: '#61DBFB', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} />ReactJS</td>
+                                    </tr>
+                                    <tr>
+                                        <td> <strong>Base de Datos</strong> </td>
+                                        <td></td>
+                                        <td><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-planetscale" width='32px' height='32px' viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" style={{ color: 'black', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }}>
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M20.993 11.63a9 9 0 0 1 -9.362 9.362l9.362 -9.362z"></path>
+                                            <path d="M12 3a9.001 9.001 0 0 1 8.166 5.211l-11.955 11.955a9 9 0 0 1 3.789 -17.166z"></path>
+                                            <path d="M12 12l-6 6"></path>
+                                        </svg>MySql con PlanetScale</td>
+                                    </tr>
+                                    <tr className='complemetos'>
+                                        <td> <strong>Complementos</strong> </td>
+                                        <td></td>
+                                        <td >
+                                            <IconBrandGit width='32px' height='32px' style={{ color: '#f96926', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} />Git
+                                            <br />
+                                            <IconBrandHtml5 width='32px' height='32px' style={{ color: '#f96926', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} />HTML
+
+                                            <br />
+                                            <IconBrandCss3 width='32px' height='32px' style={{ color: '#61DBFB', margin: '18px', backgroundColor: 'white', borderRadius: '15px' }} />CSS
+
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+
         </section>
     );
 }
