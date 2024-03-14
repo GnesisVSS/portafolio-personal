a76461339a0409a0e17027da6acad5ccb30c0c56
GnesisVSS <gnesis.vss@gmail.com>
2024-03-13 21:52:08 -0300

    prueba de uso de api con url de la api con json y adaptacion de los elementos dentro de las cards

--- src/api/receta.api.js
+++ src/api/receta.api.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 // url de recetas para base de datos local
 
-let url_recetas = 'https://bdapirest.netlify.app/.netlify/functions/api/api/receta';
-let url_ingr = 'https://bdapirest.netlify.app/.netlify/functions/api/api/ingredientes';
+// let url_recetas = 'https://bdapirest.netlify.app/.netlify/functions/api/api/receta';
+// let url_ingr = 'https://bdapirest.netlify.app/.netlify/functions/api/api/ingredientes';
 let url_guardadas = 'https://bdapirest.netlify.app/.netlify/functions/api/api/recetaguardada';
 let url_rec_usuario = 'https://bdapirest.netlify.app/.netlify/functions/api/api/recetausuario';
 let url_guardadasDet = 'https://bdapirest.netlify.app/.netlify/functions/api/api/recetaguardadaDet';
 let url_rec_usuarioDet = 'https://bdapirest.netlify.app/.netlify/functions/api/api/recetausuarioDet';
 
 // url de recetas para json
-// let url_recetas = 'https://apiprueba.netlify.app/.netlify/functions/api/recetas';
-// let url_ingr = 'https://apiprueba.netlify.app/.netlify/functions/api/ingredientes';
+let url_recetas = 'https://apiprueba.netlify.app/.netlify/functions/api/recetas';
+let url_ingr = 'https://apiprueba.netlify.app/.netlify/functions/api/ingredientes';
 
 // Mostrar recetas
 export const mostrarRecetas = async () =>
--- src/pages/Recetas/VistaDetalleRecetas.jsx
+++ src/pages/Recetas/VistaDetalleRecetas.jsx
@@ -28,18 +28,37 @@ const DetalleRecetas = () => {
 
     }, [])
 
-    function separarParrafoConNumeros(parrafo) {
-        // Utilizamos una expresión regular para buscar números seguidos de un punto en el párrafo
-        var regex = /\d+\./g;
+    // eslint-disable-next-line react/prop-types
+    function SepararParrafoConNumeros({ preparacion }) {
+        // Verificar si descripcion no es undefined
+        if (preparacion) {
+            // Utilizamos una expresión regular para buscar números seguidos de un punto en el párrafo
+            var regex = /\d+\./g;
 
-        // Reemplazamos cada número seguido de un punto encontrado por el número seguido de un salto de línea
-        var parrafoSeparado = parrafo.replace(regex, function (match) {
-            return '\n' + match;
-        });
+            // Reemplazamos cada número seguido de un punto encontrado por el número seguido de un salto de línea
+            // eslint-disable-next-line react/prop-types
+            var parrafoSeparado = preparacion.replace(regex, function (match) {
+                return '\n' + match;
+            });
 
-        return parrafoSeparado.trim();
+            // Dividir el párrafo en líneas basadas en saltos de línea y mapear cada línea
+            const lineas = parrafoSeparado.split('\n').map((linea, index) => (
+                // Por cada línea, creamos un <span> con el contenido de la línea y una etiqueta <br /> al final para generar un salto de línea.
+                <span key={index}>
+                    {linea}
+                    <br />
+                </span>
+            ));
+
+            // Devolver el array de líneas
+            return lineas;
+        } else {
+            // Si descripcion es undefined, retornar un mensaje de error o algo que indique que no hay descripción disponible
+            return <span>Cargando...</span>;
+        }
     }
 
+    console.log(ingr)
 
     return (
         <section>
@@ -49,71 +68,53 @@ const DetalleRecetas = () => {
             <div className='container mx-auto py-4'>
                 <div>
                     <div>
-                        {recetas.map(objeto => (
-                            <div key={objeto.id} className='py-5 row justify-content-center'>
-                                <div className='col-5 col-sm-6' style={{ textAlign: 'center' }}>
-                                    <img src={objeto.imgUrl} style={{ maxHeight: 400, maxWidth: 500 }} />
-                                </div>
-                                <div className='col-5 col-sm-6'>
-                                    <h1> {objeto.nombre}</h1>
-                                    <hr style={{ borderTop: "1px solid black" }} />
-                                    <h4> <AccessTimeIcon /> {objeto.tiempoPreparacion} minutos <GroupIcon /> {objeto.porciones} porciones</h4>
-                                    <h5 className='py-5'> Categoria <Chip label={objeto.categoria} /> </h5>
-
-                                </div>
-
+                        <div key={recetas.id} className='py-5 row justify-content-center'>
+                            <div className='col-5 col-sm-6' style={{ textAlign: 'center' }}>
+                                <img src={recetas.imgUrl} style={{ maxHeight: 400, maxWidth: 500 }} />
+                            </div>
+                            <div className='col-5 col-sm-6'>
+                                <h1> {recetas.nombre}</h1>
+                                <hr style={{ borderTop: "1px solid black" }} />
+                                <h4> <AccessTimeIcon /> {recetas.tiempoPreparacion} minutos <GroupIcon /> {recetas.porciones} porciones</h4>
+                                <h5 className='py-5'> Categoria <Chip label={recetas.categoria} /> </h5>
 
                             </div>
 
-                        ))}
+
+                        </div>
                         {/* <p className='py-4'>{recetas[0].nombre_receta}</p> */}
                     </div>
                     <div>
-                        {recetas.map(objeto => (
-                            <div key={objeto.id} className='py-5 row justify-content-center'>
-                                <div className='col-5 col-sm-6' style={{ textAlign: 'left' }}>
-                                    <div style={{border: '1px solid black'}}  className='p-5'>
-                                        <h1 style={{paddingBottom:'1.5rem'}}>Ingredientes</h1>
-                                        {ingr.map(objeto => (
-                                            <div key={objeto.id}>
-                                                <p> <strong>{objeto.nombre}</strong>  {objeto.cantidad === undefined ? '' : objeto.cantidad} {objeto.unidad === undefined ? '' : objeto.unidad} </p>
-                                            </div>
-                                        ))}
-                                    </div>
+                        <div key={recetas.id} className='py-5 row justify-content-center'>
+                            <div className='col-5 col-sm-6' style={{ textAlign: 'left' }}>
+                                <div style={{ border: '1px solid black' }} className='p-5'>
+                                    <h1 style={{ paddingBottom: '1.5rem' }}>Ingredientes</h1>
+                                    {ingr.map(objeto => (
+                                        <div key={objeto.id}>
+                                            <p> <strong>{objeto.nombre}</strong>  {objeto.cantidad === undefined ? '' : objeto.cantidad} {objeto.unidad === undefined ? '' : objeto.unidad} </p>
+                                        </div>
+                                    ))}
                                 </div>
-                                <div className='col-5 col-sm-6'>
-                                    <div className='p-5' style={{border: '1px solid black'}}>
+                            </div>
+                            <div className='col-5 col-sm-6'>
+                                <div className='p-5' style={{ border: '1px solid black' }}>
                                     <h1>Preparación</h1>
-                                        {recetas.map(objeto => {
-                                            var parrafo = objeto.preparacion;
-                                            var parrafosep = separarParrafoConNumeros(parrafo)
-                                            console.log(parrafosep);
-                                            return (
-                                                <div key={objeto.id} className='py-4' style={{wordWrap: "break-word"}}>
-                                                    {/* dividimos el párrafo en líneas utilizando el método split('\n'), que divide el texto en un arreglo de líneas en función del carácter de salto de línea \n. */}
-                                                    <p>{parrafosep.split('\n').map((linea, index) => (
-                                                        //Por cada línea, creamos un <span> con el contenido de la línea y una etiqueta <br /> al final para generar un salto de línea.
-                                                        <span key={index}>
-                                                            {linea}
-                                                            <br />
-                                                        </span>
-                                                    ))}</p>
-                                                </div>
-                                            );
 
-                                        })}
-                                        {/* <p className='py-4'>{recetas[0].nombre_receta}</p> */}
+                                    <div key={recetas.id} className='py-4' style={{ wordWrap: "break-word" }}>
+                                        {/* dividimos el párrafo en líneas utilizando el método split('\n'), que divide el texto en un arreglo de líneas en función del carácter de salto de línea \n. */}
+                                        <SepararParrafoConNumeros preparacion={recetas.preparacion} />
                                     </div>
+                                    {/* <p className='py-4'>{recetas[0].nombre_receta}</p> */}
                                 </div>
-
-
                             </div>
 
-                        ))}
+
+                        </div>
+
                         {/* <p className='py-4'>{recetas[0].nombre_receta}</p> */}
                     </div>
 
-                    
+
 
                 </div>
             </div >
