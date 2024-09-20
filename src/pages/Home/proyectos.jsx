dee525865c973049c67da170e24586b326561876
unknown <gnesis.vss@gmail.com>
2024-09-20 00:41:33 -0300

    se cambió el orden de entre el proyecto de alkemy y generation

--- src/pages/Home/proyectos.jsx
+++ src/pages/Home/proyectos.jsx
@@ -229,44 +229,6 @@ const Proyectos = () => {
 
 
                                 </div>
-                                <div className='col-sm-12 col-md-12 col-lg-6 align-self-center py-3' >
-                                    <Atropos>
-                                        <div className="card-prev-rec" data-atropos-offset="-3">
-                                            <div className="tools">
-                                                <div className="circle">
-                                                    <span className="red box"></span>
-                                                </div>
-                                                <div className="circle">
-                                                    <span className="yellow box"></span>
-                                                </div>
-                                                <div className="circle">
-                                                    <span className="green box"></span>
-                                                </div>
-                                                <span className='box-titulo'>
-                                                    <IconLock className='IconLock' style={{margin: '0 8px'}}/> Ecotourist
-                                                </span>
-                                            </div>
-                                            <div className="card__content">
-
-                                                <div className="card__background_proximamente" style={{
-                                                    backgroundImage: 'url(/img/EcoTourist/TrekkingPortada.png)'
-                                                }}></div>
-
-                                                <button className='button-demo_proximamente' disabled="true" style={{
-                                                    position: 'relative',
-                                                    zIndex: 2,
-                                                }}>
-                                                    <svg className='svg-demo_proximamente' height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M0 0h24v24H0z" fill="none"></path>
-                                                        <path d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" fill="currentColor"></path>
-                                                    </svg>
-                                                    <a className="span-demo_proximamente"></a>
-                                                </button>
-
-                                            </div>
-                                        </div>
-                                    </Atropos>
-                                </div>
                                 {/* TalentAlke */}
                                 <div className='col-sm-12 col-md-12 col-lg-6 align-self-center py-3' >
                                     <Atropos>
@@ -306,6 +268,45 @@ const Proyectos = () => {
                                         </div>
                                     </Atropos>
                                 </div>
+                                <div className='col-sm-12 col-md-12 col-lg-6 align-self-center py-3' >
+                                    <Atropos>
+                                        <div className="card-prev-rec" data-atropos-offset="-3">
+                                            <div className="tools">
+                                                <div className="circle">
+                                                    <span className="red box"></span>
+                                                </div>
+                                                <div className="circle">
+                                                    <span className="yellow box"></span>
+                                                </div>
+                                                <div className="circle">
+                                                    <span className="green box"></span>
+                                                </div>
+                                                <span className='box-titulo'>
+                                                    <IconLock className='IconLock' style={{margin: '0 8px'}}/> Ecotourist
+                                                </span>
+                                            </div>
+                                            <div className="card__content">
+
+                                                <div className="card__background_proximamente" style={{
+                                                    backgroundImage: 'url(/img/EcoTourist/TrekkingPortada.png)'
+                                                }}></div>
+
+                                                <button className='button-demo_proximamente' disabled="true" style={{
+                                                    position: 'relative',
+                                                    zIndex: 2,
+                                                }}>
+                                                    <svg className='svg-demo_proximamente' height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" fill="currentColor"></path>
+                                                    </svg>
+                                                    <a className="span-demo_proximamente"></a>
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                    </Atropos>
+                                </div>
+                                
 
                             </div>
                         </div>
